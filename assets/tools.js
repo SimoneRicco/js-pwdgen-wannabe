0d1e348c39ed3679e384d321d30ee8b631001596
SimoneRicco <smnrcc0@gmail.com>
2023-04-04 13:17:33 +0200

    end

---
+++ assets/tools.js
@@ -0,0 +1,4 @@
+const name = prompt("Inserisci il tuo nome");
+const surname = prompt("Inserisci il tuo cognome");
+const favouriteColor = prompt("Inserisci il tuo colore preferito");
+document.getElementById("output").innerHTML = "La password generata e': " + name + surname + favouriteColor + "23";
